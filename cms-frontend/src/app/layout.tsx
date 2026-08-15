64b2d4f95e98946833ffe9d5114fd1cfa8f7d005
hoangnt142 <hoangnt142@gmail.com>
2026-08-15 15:55:38 +0700

    Fix shared product image viewer CSS

--- cms-frontend/src/app/layout.tsx
+++ cms-frontend/src/app/layout.tsx
@@ -191,6 +191,7 @@ function buildHeadMarkup({
     '<link rel="preconnect" href="https://cdn.x24sport.vn" crossorigin="anonymous">',
     '<link rel="stylesheet" href="/styles/contact-bar.css?v=20260814d">',
     ...stylesheets.map((href) => `<link rel="stylesheet" href="${href}">`),
+    '<link rel="stylesheet" href="/styles/product-viewer.css?v=20260815a">',
     googleTagManagerId
       ? `<script id="google-tag-manager">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${googleTagManagerId}');</script>`
       : '',
